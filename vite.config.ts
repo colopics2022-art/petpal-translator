a6327fa328cc1b68299ead244c2e4fe63139b6c0
colopics2022-art <colopics2022@gmail.com>
2026-03-23 08:32:18 -0700

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base:'/petpal-translator/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
